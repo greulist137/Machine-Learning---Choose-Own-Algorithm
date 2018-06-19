e4ecb7b5d61c6d0976e5b40e4ab7f6e6bce0207e
Steven Greulich <greulich.steven@gmail.com>
2018-06-19 05:57:55 -0700

    Commit - Completion of RandomForestClassifier
    The purpose of this commit is for the implementation of the RandomForestClassifier for the Choose your own algorithm. Accuracy score with no changes to the default parameters is roughly 90.8%

--- ud120-projects-master/choose_your_own/your_algorithm.py
+++ ud120-projects-master/choose_your_own/your_algorithm.py
@@ -3,6 +3,8 @@
 import matplotlib.pyplot as plt
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
@@ -27,16 +29,20 @@ plt.ylabel("grade")
 plt.show()
 ################################################################################
 
+#Create a RandomForestClassifier and fit it to the features/labels training data
+clf = RandomForestClassifier()
+clf.fit(features_train, labels_train)
 
-### your code here!  name your classifier object clf if you want the 
-### visualization code (prettyPicture) to show you the decision boundary
+pred = clf.predict(features_test)
 
+#Print Prediction result given features_test set
+print('Prediction')
+print(pred)
 
+score = accuracy_score(pred, labels_test)
 
-
-
-
-
+print('Score')
+print(score)
 
 try:
     prettyPicture(clf, features_test, labels_test)
